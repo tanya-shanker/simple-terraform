821ac290f163032c5f6222535274ec6098c73f3e
tanya-shanker <85172525+tanya-shanker@users.noreply.github.com>
2024-03-07 19:11:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
 
+variable "resource_group_name" {
+  description = "Where to create resources"
+  default = "Default"
+}
+
+variable "vpc_name" {
+  description = "vpc name"
+default = "simple-vpc"
+}
